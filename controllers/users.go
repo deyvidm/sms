8e53798e8b36d0a4932f6affaa3f528bdbf588ea
deyvid <deyvidmitkov@gmail.com>
2023-05-09 14:17:32 +0200

    small models update

--- controllers/users.go
+++ controllers/users.go
@@ -40,5 +40,16 @@ func SignIn(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "validated!", "data": input})
+	u := models.User{}
+	u.Username = input.Username
+	u.Password = input.Password
+
+	token, err := models.LoginCheck(u.Username, u.Password)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "incorrect login details"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token})
 }
--- models/contact.go
+++ models/contact.go
@@ -1,6 +1,9 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type Contact struct {
+	gorm.Model
 	FirstName string `form:"first_name" json:"first_name" binding:"required,min=3,max=50"`
 	LastName  string `form:"last_name" json:"last_name" binding:"required,min=3,max=50"`
 	Mobile    string `form:"phone" json:"phone" binding:"required,min=10,max=10"`
