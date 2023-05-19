5f2a9a13656424425bc0333e453b8eeb504a73e6
deyvid <deyvidmitkov@gmail.com>
2023-05-19 09:32:24 -0400

    little jsondump util

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,18 @@
 package utils
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 func Ptr[T any](v T) *T {
 	return &v
+}
+
+func JSONDump(x interface{}) {
+	b, err := json.MarshalIndent(x, "", "  ")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Print(string(b))
 }
