38e624a43546b46707f38c18201d949f911d1eb2
deyvid <deyvidmitkov@gmail.com>
2023-05-19 07:50:56 -0400

    cleaned up client

--- self-client/client.go
+++ self-client/client.go
@@ -4,36 +4,30 @@ import (
 	"github.com/deyvidm/sms-asynq/log"
 	"github.com/deyvidm/sms-asynq/tasks"
 	"github.com/hibiken/asynq"
+	"github.com/sirupsen/logrus"
 )
 
+var logger *logrus.Logger
+
 func main() {
-	logger := log.GetLogger()
+	logger = log.GetLogger()
 	logger.Info("Test Client initiated")
 	client := asynq.NewClient(asynq.RedisClientOpt{Addr: "localhost:6379"})
 
-	var err error
-	var info *asynq.TaskInfo
-	t1, err := tasks.NewInviteTask("123", "+11234567890", "tiger butts")
-	if err != nil {
-		logger.Fatal(err)
-	}
+	var t *asynq.Task
 
-	// logger.Infof("Enquing Invite %s", t1.Type())
-	// info, err = client.Enqueue(t1)
-	// if err != nil {
-	// 	logger.Fatal(err)
-	// }
-	// logger.Infof("Successfully enqueued task %s with ID %v ", info.Type, info.ID)
+	t, _ = tasks.NewInviteTask("123", "+11234567890", "tiger butts")
+	logAndEnqueue(client, t)
 
-	logger.Infof("Enquing Response %s", t1.Type())
-	t2, err := tasks.NewReponseTask("+11234567890", "YOOO")
-	if err != nil {
-		logger.Fatal(err)
-	}
-	info, err = client.Enqueue(t2)
+	t, _ = tasks.NewReponseTask("+11234567890", "YOOO")
+	logAndEnqueue(client, t)
+
+}
+
+func logAndEnqueue(c *asynq.Client, t *asynq.Task) {
+	info, err := c.Enqueue(t)
 	if err != nil {
 		logger.Fatal(err)
 	}
 	logger.Infof("Successfully enqueued task %s with ID %v ", info.Type, info.ID)
-
 }
