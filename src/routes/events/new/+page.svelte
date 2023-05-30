5b3cefb93fd8d30aa03c26cfa288ec5578f3d3e3
deyvid <deyvidmitkov@gmail.com>
2023-05-30 12:37:55 -0400

    rm'd big dumb comment

--- src/routes/events/new/+page.svelte
+++ src/routes/events/new/+page.svelte
@@ -1,65 +1,47 @@
 <script lang="ts">
   import ContactList from "$lib/ContactList.svelte";
-    import { apiClient } from "$lib/gin";
+  import { apiClient } from "$lib/gin";
+  import { onMount } from "svelte";
+
+  /** @type {import('./$types').PageData} */
+  export let data;
 
   let title: string;
   let invite_body: string;
-  let recipients = new Array();
+  let available = new Array();
+  let invited = new Array();
   let submitButton: HTMLButtonElement;
-  
+  let contacts = new Array();
+
   function handleMessage(event) {
-    recipients = event.detail.recipients;
+    available = event.detail.recipients;
   }
 
-function extractIDs(iterable) {
-  // return Array.from(iterable, (node) => console.log(node.id));
-}
+  function extractIDs(iterable) {
+    // return Array.from(iterable, (node) => console.log(node.id));
+  }
 
   async function create() {
-    submitButton.classList.add("loading")
-    const body = await apiClient.post("/events/new",{
-      title: title,
-      invite_body: invite_body,
-      contacts: extractIDs(recipients)
-    }).then((body)=>{
-        submitButton.classList.remove("loading")
-        submitButton.classList.add("btn-disabled")
-        submitButton.classList.add("btn-primary")
-        submitButton.textContent = "Success"
-    });
-
-    // createEvent(<EventRecord>{
-    //     organizer: pb.authStore.model?.id,
-    //     title: formTitle,
-    //     description: content,
-    //     capacity: 666,
-    //     start_date: d.toISOString(),
-    //     end_date: d.toISOString(),
-    //     send_invite_date: d.toISOString(),
-    //     status: EventStatusOptions.active,
-    // }).then((eventRecord) => {
-    //     recipients.forEach((r) => {
-    //         pb.collection('attendee')
-    //             .create(<AttendeeRecord>{
-    //                 event: eventRecord.id,
-    //                 contact: r.id,
-    //                 status: AttendeeStatusOptions['sending-invite'],
-    //                 paid: false,
-    //             })
-    //             .catch((error) => {
-    //                 console.log(error);
-    //             });
-    //     });
-    // }).then(()=>{
-    //     submitButton.classList.remove("loading")
-    //     submitButton.classList.add("btn-disabled")
-    //     submitButton.textContent = "Success"
-    // });
+    submitButton.classList.add("loading");
+    const body = await apiClient
+      .post("/events/new", {
+        title: title,
+        invite_body: invite_body,
+        contacts: extractIDs(available),
+      })
+      .then((body) => {
+        submitButton.classList.remove("loading");
+        submitButton.classList.add("btn-disabled");
+        submitButton.classList.add("btn-primary");
+        submitButton.textContent = "Success";
+      });
   }
-  /** @type {import('./$types').PageData} */
-  export let data;
-  let contacts = new Array();
-  contacts = data.contacts;
+
+  onMount(async () => {
+    available = data.contacts;
+    console.log("bam ", available.length)
+  });
+
 </script>
 
 <h2 class="mb-10 text-4xl font-extrabold dark:text-white">Create New Event</h2>
@@ -71,7 +53,7 @@ function extractIDs(iterable) {
         <span class="text-xl font-bold text-xllabel-text">Title</span>
       </label>
       <input
-      bind:value={title}
+        bind:value={title}
         type="text"
         name="title"
         placeholder="Sunday Funday St Paddy's Vball"
@@ -83,16 +65,27 @@ function extractIDs(iterable) {
         <span class="text-xl font-bold text-xllabel-text">SMS Content</span>
       </label>
       <textarea
-      bind:value={invite_body}
+        bind:value={invite_body}
         name="content"
         class="w-full h-96 pb-10 textarea textarea-bordered"
       />
     </div>
 
-    <ContactList {contacts} on:message={handleMessage} />
-    <input type="hidden" name="recipients"  value={recipients}/>
-    {#if recipients.length > 0}
-      <button bind:this={submitButton}  class="mt-12 w-1/4 btn btn-active">Create</button>
+    <div class="flex space-x-4">
+      <span>
+        <span class="text-xl font-bold text-xllabel-text">Available</span>
+        <ContactList contacts={available} on:message={handleMessage} />
+      </span>
+      <span>
+        <span class="text-xl font-bold text-xllabel-text">Invited</span>
+        <ContactList contacts={invited} on:message={handleMessage} />
+      </span>
+    </div>
+    <input type="hidden" name="recipients" value={available} />
+    {#if available.length > 0}
+      <button bind:this={submitButton} class="mt-12 w-1/4 btn btn-active"
+        >Create</button
+      >
     {/if}
   </form>
 </div>
