91189c3091b402601ccd73a613b0132df36834c7
deyvid <deyvidmitkov@gmail.com>
2023-10-07 11:46:58 -0400

    prepping monorepo

--- src/routes/contacts/new/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import ContactForm from "$lib/ContactForm.svelte";
-
-</script>
-<div class="w-3/4 p-5 shadow-xl">
-    Disabled
-    <!-- <ContactForm/> -->
-</div>
--- src/routes/logout/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { enhance } from '$app/forms';
-</script>
-
-<div class="absolute right-2">
-<form use:enhance method="POST" action="?/logout">
-    <button class="btn btn-outline-danger">Confirm Loggin Out</button>
-</form>
-</div>
